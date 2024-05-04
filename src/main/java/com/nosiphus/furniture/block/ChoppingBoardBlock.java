9b988e4d52f2acbb70fa9ecef51ba54137acc215
Sean Crain <38871906+seancrain@users.noreply.github.com>
2024-05-03 22:00:15 -0500

    Resume work on chopping boards

--- src/main/java/com/nosiphus/furniture/block/ChoppingBoardBlock.java
+++ src/main/java/com/nosiphus/furniture/block/ChoppingBoardBlock.java
@@ -4,19 +4,35 @@ import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.mrcrayfish.furniture.block.FurnitureHorizontalBlock;
 import com.mrcrayfish.furniture.util.VoxelShapeHelper;
+import com.nosiphus.furniture.blockentity.ChoppingBoardBlockEntity;
+import com.nosiphus.furniture.core.ModItems;
+import com.nosiphus.furniture.recipe.ChoppingRecipe;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.world.Containers;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.EntityBlock;
+import net.minecraft.world.level.block.RenderShape;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.Vec3;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 
+import javax.annotation.Nullable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
-public class ChoppingBoardBlock extends FurnitureHorizontalBlock
+public class ChoppingBoardBlock extends FurnitureHorizontalBlock implements EntityBlock
 {
 
     public final ImmutableMap<BlockState, VoxelShape> SHAPES;
@@ -51,6 +67,67 @@ public class ChoppingBoardBlock extends FurnitureHorizontalBlock
     @Override
     public VoxelShape getOcclusionShape(BlockState state, BlockGetter reader, BlockPos pos) {
         return SHAPES.get(state);
+    }
+
+    @Override
+    public RenderShape getRenderShape(BlockState state) {
+        return RenderShape.MODEL;
+    }
+
+    @Override
+    public void onRemove(BlockState state, Level level, BlockPos pos, BlockState newState, boolean isMoving) {
+        if (state.getBlock() != newState.getBlock()) {
+            if(level.getBlockEntity(pos) instanceof ChoppingBoardBlockEntity blockEntity) {
+                Containers.dropContents(level, pos, blockEntity.getChoppingBoard());
+            }
+        }
+        super.onRemove(state, level, pos, newState, isMoving);
+    }
+
+    @Override
+    public InteractionResult use(BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult result) {
+        if(!level.isClientSide() && result.getDirection() == Direction.UP) {
+            if(level.getBlockEntity(pos) instanceof ChoppingBoardBlockEntity blockEntity) {
+                ItemStack heldItem = player.getItemInHand(hand);
+                ItemStack choppingItem = blockEntity.getItem(0);
+                if(heldItem.getItem() == ModItems.KNIFE.get()) {
+                    Optional<ChoppingRecipe> recipe = blockEntity.findMatchingRecipe(choppingItem);
+
+                    blockEntity.chopItem(choppingItem, this.getPosition(result, pos));
+                } else if (!heldItem.isEmpty()) {
+                    Optional<ChoppingRecipe> recipe = blockEntity.findMatchingRecipe(heldItem);
+                    if(recipe.isPresent()) {
+                        if(blockEntity.addItem(heldItem, this.getPosition(result, pos))) {
+                            if(!player.getAbilities().instabuild) {
+                                heldItem.shrink(1);
+                            }
+                        }
+                    } else {
+                        blockEntity.removeItem(this.getPosition(result, pos));
+                    }
+                } else {
+                    blockEntity.removeItem(this.getPosition(result, pos));
+                }
+            }
+        }
+        return InteractionResult.SUCCESS;
+    }
+
+    private int getPosition(BlockHitResult hit, BlockPos pos) {
+        int position = 0;
+        return position;
+    }
+
+    @Nullable
+    @Override
+    public BlockEntity newBlockEntity(BlockPos pos, BlockState state) {
+        return new ChoppingBoardBlockEntity(pos, state);
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder)
+    {
+        super.createBlockStateDefinition(builder);
     }
 
 }
---
+++ src/main/java/com/nosiphus/furniture/client/renderer/blockentity/ChoppingBoardBlockEntityRenderer.java
@@ -0,0 +1,39 @@
+package com.nosiphus.furniture.client.renderer.blockentity;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.math.Vector3f;
+import com.nosiphus.furniture.blockentity.ChoppingBoardBlockEntity;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.block.model.ItemTransforms;
+import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
+import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
+import net.minecraft.core.NonNullList;
+import net.minecraft.world.item.ItemStack;
+
+public class ChoppingBoardBlockEntityRenderer implements BlockEntityRenderer<ChoppingBoardBlockEntity> {
+
+    public ChoppingBoardBlockEntityRenderer(BlockEntityRendererProvider.Context context) {}
+
+    @Override
+    public void render(ChoppingBoardBlockEntity blockEntity, float partialTicks, PoseStack poseStack, MultiBufferSource source, int light, int overlay) {
+        NonNullList<ItemStack> choppingBoard = blockEntity.getChoppingBoard();
+        ItemStack stack = choppingBoard.get(0);
+        if(!stack.isEmpty()) {
+
+            poseStack.pushPose();
+
+            float xOffset = 0.0F;
+            float zOffset = 0.0F;
+
+            poseStack.translate(0.5F + xOffset, 0.02F, 0.3F + zOffset);
+            poseStack.mulPose(Vector3f.XP.rotation(0.1F * -90F));
+            poseStack.mulPose(Vector3f.ZP.rotation(180F));
+            poseStack.scale(0, -0.15F, 0);
+            Minecraft.getInstance().getItemRenderer().renderStatic(stack, ItemTransforms.TransformType.FIXED, light, overlay, poseStack, source, 0);
+
+            poseStack.popPose();
+        }
+    }
+
+}
