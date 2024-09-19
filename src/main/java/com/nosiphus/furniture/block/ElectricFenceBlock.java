5b16496e2bebaaf0ad8797a5fa85144c02270340
Sean Crain <38871906+seancrain@users.noreply.github.com>
2024-09-19 18:20:36 -0500

    Add proper block sizing to electric fence

--- src/main/java/com/nosiphus/furniture/block/ElectricFenceBlock.java
+++ src/main/java/com/nosiphus/furniture/block/ElectricFenceBlock.java
@@ -1,5 +1,8 @@
 package com.nosiphus.furniture.block;
 
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.mrcrayfish.furniture.util.VoxelShapeHelper;
 import com.nosiphus.furniture.common.ModDamageTypes;
 import com.nosiphus.furniture.core.ModSounds;
 import net.minecraft.core.BlockPos;
@@ -11,14 +14,78 @@ import net.minecraft.world.entity.LightningBolt;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.monster.Creeper;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.FenceBlock;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ElectricFenceBlock extends FenceBlock {
 
+    public final ImmutableMap<BlockState, VoxelShape> SHAPES;
+
     public ElectricFenceBlock(Properties properties) {
         super(properties);
+        this.registerDefaultState(this.getStateDefinition().any());
+        SHAPES = this.generateShapes(this.getStateDefinition().getPossibleStates());
+    }
+
+    private ImmutableMap<BlockState, VoxelShape> generateShapes(ImmutableList<BlockState> states) {
+
+        final VoxelShape POST = Block.box(6.5, 0.0, 6.5, 9.5, 16.0, 9.5);
+
+        final VoxelShape NORTH_SIDE = Block.box(7.3, 0.0, 0.0, 8.7, 16.0, 8.0);
+        final VoxelShape EAST_SIDE = Block.box(8.0, 0.0, 7.3, 16.0, 16.0, 8.7);
+        final VoxelShape SOUTH_SIDE = Block.box(7.3, 0.0, 8.0, 8.7, 16.0, 16.0);
+        final VoxelShape WEST_SIDE = Block.box(0.0, 0.0, 7.3, 8.0, 16.0, 8.7);
+
+        ImmutableMap.Builder<BlockState, VoxelShape> builder = new ImmutableMap.Builder<>();
+        for(BlockState state : states) {
+
+            boolean north = state.getValue(NORTH);
+            boolean east = state.getValue(EAST);
+            boolean south = state.getValue(SOUTH);
+            boolean west = state.getValue(WEST);
+
+            List<VoxelShape> shapes = new ArrayList<>();
+            shapes.add(POST);
+
+            if(north) {
+                shapes.add(NORTH_SIDE);
+            }
+
+            if(east) {
+                shapes.add(EAST_SIDE);
+            }
+
+            if(south) {
+                shapes.add(SOUTH_SIDE);
+            }
+
+            if(west) {
+                shapes.add(WEST_SIDE);
+            }
+
+            builder.put(state, VoxelShapeHelper.combineAll(shapes));
+        }
+        return builder.build();
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState state, BlockGetter reader, BlockPos pos, CollisionContext context)
+    {
+        return SHAPES.get(state);
+    }
+
+    @Override
+    public VoxelShape getCollisionShape(BlockState state, BlockGetter reader, BlockPos pos, CollisionContext context)
+    {
+        return SHAPES.get(state);
     }
 
     @Override
